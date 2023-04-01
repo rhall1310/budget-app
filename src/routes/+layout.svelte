d618df03e23f769e6fca7d2a5288ad60c8829389
Rob Hall <r-narch@hotmail.com>
2023-04-01 18:58:20 +0100

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <body>
 	<aside>
+		<h1><a href="/"> Budget Calculator</a></h1>
 		<nav>
-			<a href="/">Home</a>
-			<a href="/about">About</a>
 			<a href="/settings">Settings</a>
+			<a href="/about">About</a>
 		</nav>
 	</aside>
 
@@ -14,15 +14,20 @@
 
 <style>
 	body {
-		display: flex;
 		margin: 0;
 	}
 	aside {
-		background-color: aqua;
+		background-color: rgb(112, 201, 201);
+		display: flex;
+		align-items: center;
+		gap: 1rem;
 	}
 	nav {
 		display: flex;
-		flex-direction: column;
-		padding-right: 2rem;
+		flex-direction: row;
+		gap: 1rem;
+	}
+	a {
+		text-decoration: none;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,6 @@
 <p>You can use this tool to calculate your monthly budget</p>
 <input type="text" name="income" id="" />
 <label for="income">What is your monthly income?</label>
+<input type="text" name="expenditure" id="" />
+<label for="income">What is your monthly expenditure?</label>
+<input type="text" name="total" id="" value="" />
