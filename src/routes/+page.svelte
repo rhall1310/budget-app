c311f26f3b95f0b17858144c6cb9cdb55a0edd4d
Rob Hall <r-narch@hotmail.com>
2023-04-02 12:58:59 +0100

    binding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
+<script>
+	export let expend;
+	export let income;
+</script>
+
 <h1>Budget Calculator</h1>
 <p>You can use this tool to calculate your monthly budget</p>
-<input type="text" name="income" id="" />
-<label for="income">What is your monthly income?</label>
-<input type="text" name="expenditure" id="" />
+<div>
+	<label for="income">What is your monthly income?</label>
+	<input type="text" name="expenditure" id="" bind:value={income} />
+</div>
 <label for="income">What is your monthly expenditure?</label>
-<input type="text" name="total" id="" value="" />
+<input type="text" name="total" id="" bind:value={expend} />
+<p>Your total savings are: {income - expend}</p>
