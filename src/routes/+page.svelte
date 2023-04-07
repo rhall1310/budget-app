022e43eaea7d7e3bef3b0a4dc0540ae02fe01be7
Rob Hall <r-narch@hotmail.com>
2023-04-07 17:23:59 +0100

    redundant divs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,21 +31,21 @@
 		<input type="text" name="add-income" placeholder="Add income source" bind:value={newIncome} />
 		<button on:click={addIncome(newIncome)}>+</button>
 	</div>
-	<div>
-		<div>
-			<p>What is your monthly spending?</p>
-			{#each expendCount as exp, index}
-				<li>
-					{index + 1}.
-					<label for="income">{exp}:</label>
-					<input type="text" name="income" bind:value={expend} />
-				</li>
-			{/each}
 
-			<input type="text" name="add-expend" placeholder="Add expenditure" bind:value={newExpend} />
-			<button on:click={addExpenditure(newExpend)}>+</button>
-		</div>
+	<div>
+		<p>What is your monthly spending?</p>
+		{#each expendCount as exp, index}
+			<li>
+				{index + 1}.
+				<label for="income">{exp}:</label>
+				<input type="text" name="income" bind:value={expend} />
+			</li>
+		{/each}
+
+		<input type="text" name="add-expend" placeholder="Add expenditure" bind:value={newExpend} />
+		<button on:click={addExpenditure(newExpend)}>+</button>
 	</div>
+
 	<p>Your total savings are: {income - expend}</p>
 </main>
 
@@ -55,5 +55,9 @@
 	}
 	li {
 		list-style: none;
+	}
+
+	input {
+		margin: 0.2rem;
 	}
 </style>
