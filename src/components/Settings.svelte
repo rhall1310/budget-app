4f09cd76dabdb293bf65eccdaf5088e163e90b13
Rob Hall <r-narch@hotmail.com>
2023-05-15 14:59:23 +0100

    modal changes

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -3,46 +3,28 @@
 	export let show = false;
 </script>
 
-<main>
-	<div>
-		<button on:click={() => (show = !show)}>Close X</button>
-		<h1>Settings</h1>
+<div>
+	<button on:click={() => (show = !show)}>Close X</button>
+	<h1>Settings</h1>
 
-		<label for="period">Select budget time period</label>
-		<select name="period" id="period" bind:value={$settings.period}>
-			<option value="monthly">Monthly</option>
-			<option value="annual">Annual</option>
-			<option value="weekly">Weekly</option>
-		</select>
+	<label for="period">Select budget time period</label>
+	<select name="period" id="period" bind:value={$settings.period}>
+		<option value="monthly">Monthly</option>
+		<option value="annual">Annual</option>
+		<option value="weekly">Weekly</option>
+	</select>
 
-		<div class="settings-rows">
-			<input type="checkbox" name="darkMode" id="darkMode" bind:value={$settings.darkMode} />
-			<label for="darkMode">Dark mode?</label>
-		</div>
+	<div class="settings-rows">
+		<input type="checkbox" name="darkMode" id="darkMode" bind:value={$settings.darkMode} />
+		<label for="darkMode">Dark mode?</label>
 	</div>
-</main>
+</div>
 
 <style>
-	main {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		width: 95%;
-		height: 95%;
-		background-color: rgb(232, 238, 242);
-		z-index: 1;
-		border-radius: 1rem;
-	}
 	select {
 		margin: 1rem;
 	}
-	div {
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		align-items: center;
-	}
+
 	.settings-rows {
 		display: flex;
 		flex-direction: row;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,18 +3,29 @@
 	import Settings from '../components/Settings.svelte';
 
 	let sidebarShow = false;
-	let showSettings = false;
+
+	let dialog;
+	let showModal = false;
+
+	$: if (dialog && showModal) dialog.showModal();
 </script>
 
 <body>
-	{#if showSettings}
-		<Settings bind:show={showSettings} />
-	{/if}
+	<dialog
+		bind:this={dialog}
+		on:close={() => (showModal = false)}
+		on:click|self={() => dialog.close()}
+	>
+		<Settings />
+
+		<button on:click={() => dialog.close()}>close modal</button>
+	</dialog>
+
 	<header>
 		<aside>
 			<button on:click={() => (sidebarShow = !sidebarShow)}>Toggle Sidebar</button><Sidebar
 				bind:show={sidebarShow}
-				bind:showSettings
+				bind:showSettings={showModal}
 			/>
 		</aside>
 		<h1><a href="/"> Budget Calculator</a></h1>
@@ -26,6 +37,19 @@
 </body>
 
 <style>
+	dialog {
+		background-color: rgb(232, 238, 242);
+		z-index: 1;
+		border-radius: 1rem;
+		margin-top: 5rem;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		align-items: center;
+	}
+	dialog::backdrop {
+		background-color: darkorange;
+	}
 	body {
 		margin: 0;
 		font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode',
